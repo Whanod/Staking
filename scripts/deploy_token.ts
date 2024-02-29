787b2f7f83d5e99e1e333696ce74e39711bb3777
whanod <whanod@gmail.com>
2024-02-29 03:42:27 +0330

    add scripts for testnet

---
+++ scripts/deploy_token.ts
@@ -0,0 +1,79 @@
+import {
+  Connection,
+  Keypair,
+  PublicKey,
+  Transaction,
+  clusterApiUrl,
+  sendAndConfirmTransaction,
+} from "@solana/web3.js";
+
+import "dotenv/config";
+import { Metaplex } from "@metaplex-foundation/js";
+import { getKeypairFromEnvironment } from "@solana-developers/node-helpers";
+import {
+  createMint,
+  getOrCreateAssociatedTokenAccount,
+  mintTo,
+} from "@solana/spl-token";
+import { createCreateMetadataAccountV3Instruction } from "@metaplex-foundation/mpl-token-metadata";
+const main = async () => {
+  let senderKey = getKeypairFromEnvironment("SECRET_KEY");
+  const connection = new Connection(clusterApiUrl("devnet"), {
+    commitment: "confirmed",
+  });
+  const metaplex = new Metaplex(connection);
+  const mintAccount = await createMint(
+    connection,
+    senderKey,
+    senderKey.publicKey,
+    senderKey.publicKey,
+    6
+  );
+
+  const metadata = metaplex.nfts().pdas().metadata({ mint: mintAccount });
+  const tx = new Transaction().add(
+    createCreateMetadataAccountV3Instruction(
+      {
+        metadata: metadata,
+        mint: mintAccount,
+        mintAuthority: senderKey.publicKey,
+        payer: senderKey.publicKey,
+        updateAuthority: senderKey.publicKey,
+      },
+      {
+        createMetadataAccountArgsV3: {
+          data: {
+            name: "Furries",
+            symbol: "Fur",
+            uri: "",
+            sellerFeeBasisPoints: 0,
+            collection: null,
+            creators: null,
+            uses: null,
+          },
+          isMutable: true,
+          collectionDetails: null,
+        },
+      }
+    )
+  );
+  await sendAndConfirmTransaction(connection, tx, [senderKey]);
+  const tokenAccount = await getOrCreateAssociatedTokenAccount(
+    connection,
+    senderKey,
+    mintAccount,
+    senderKey.publicKey
+  );
+  let decimals = 1_000_000;
+  await mintTo(
+    connection,
+    senderKey,
+    mintAccount,
+    tokenAccount.address,
+    senderKey,
+    100 * decimals
+  );
+  console.log(mintAccount);
+};
+
+main();
---
+++ scripts/init.ts
@@ -0,0 +1,55 @@
+import { Connection, Keypair, clusterApiUrl } from "@solana/web3.js";
+
+import "dotenv/config";
+import { PublicKey } from "@solana/web3.js";
+import { utils } from "@project-serum/anchor";
+import { findProgramAddressSync } from "@project-serum/anchor/dist/cjs/utils/pubkey";
+import { getKeypairFromEnvironment } from "@solana-developers/node-helpers";
+import { AnchorProvider, Wallet, Program } from "@coral-xyz/anchor";
+import { IDL } from "../app/src/idl/staking";
+import { BN } from "bn.js";
+
+const main = async () => {
+  const connection = new Connection(clusterApiUrl("devnet"), {
+    commitment: "confirmed",
+  });
+
+  const tokenMintAccount = process.env.TOKEN_MINT_ACCOUNT;
+
+  const TokenOwner = getKeypairFromEnvironment("SECRET_KEY");
+  const wallet = new Wallet(TokenOwner);
+  const collectionAddress = new PublicKey(process.env.COLLECTION_ADDRESS);
+  const provider = new AnchorProvider(connection, wallet, {});
+
+  const programId = new PublicKey(process.env.PROGRAM_ID);
+  const program = new Program(IDL, programId, provider);
+  let [stake_details] = findProgramAddressSync(
+    [
+      utils.bytes.utf8.encode("stake"),
+      collectionAddress.toBytes(),
+      TokenOwner.publicKey.toBytes(),
+    ],
+    programId
+  );
+  let [token_authority] = findProgramAddressSync(
+    [utils.bytes.utf8.encode("token-authority"), stake_details.toBytes()],
+    programId
+  );
+  let [nft_authority] = PublicKey.findProgramAddressSync(
+    [utils.bytes.utf8.encode("nft-authority"), stake_details.toBytes()],
+    programId
+  );
+
+  let tx = await program.methods
+    .init(new BN(500))
+    .accounts({
+      stakeDetails: stake_details,
+      tokenMint: tokenMintAccount,
+      collectionAddress: collectionAddress,
+      nftAuthority: nft_authority,
+      tokenAuthority: token_authority,
+    })
+    .rpc({ commitment: "confirmed" });
+  console.log(tx);
+};
+main();
